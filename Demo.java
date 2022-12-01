8c5ad7de479275fff0230744a565d9917871c70c
siva <95267873+sssreddys@users.noreply.github.com>
2022-12-01 19:13:09 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,6 +1,7 @@
 public class Demo {
      public static void main(String[] args) {
         System.out.println("Hello World I am Java");
+        System.out.println("I am In cron Job")
     }  
     
 }
