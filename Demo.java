afc51fa03ef2bb256a76e60410ef8b988f4d3b47
siva <95267873+sssreddys@users.noreply.github.com>
2022-12-01 19:09:26 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,6 +1,6 @@
 public class Demo {
-    private static public static void main(String[] args) {
-        system.out.println("Hello World I am Java");
+     public static void main(String[] args) {
+        System.out.println("Hello World I am Java");
     }  
     
 }
